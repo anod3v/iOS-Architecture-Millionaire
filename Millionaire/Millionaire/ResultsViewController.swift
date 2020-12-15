52d16f113b35360a68b0c798f03efa3fe1b2162a
anod3v <anod3v@gmail.com>
2020-12-15 19:48:13 +0300

    storybord removed

---
+++ Millionaire/Millionaire/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  Millionaire
+//
+//  Created by Andrey on 15/12/2020.
+//  Copyright © 2020 Andrey Anoshkin. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Millionaire/Millionaire/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  Millionaire
+//
+//  Created by Andrey on 15/12/2020.
+//  Copyright © 2020 Andrey Anoshkin. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ResultsViewController()
+        window?.makeKeyAndVisible()
+    }
+
+
+}
+
