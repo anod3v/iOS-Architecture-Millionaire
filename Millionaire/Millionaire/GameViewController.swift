6b16929341eaef62d844801dd8c5abb2abb86f7a
anod3v <anod3v@gmail.com>
2020-12-16 22:03:23 +0300

    Custom button added

--- Millionaire/Millionaire/GameViewController.swift
+++ Millionaire/Millionaire/GameViewController.swift
@@ -9,11 +9,42 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    
+    let ButtonA: AnswerButton = {
+        let button = AnswerButton(frame: .zero, titleText: String(), answerIsCorrect: false)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let ButtonB: AnswerButton = {
+        let button = AnswerButton(frame: .zero, titleText: String(), answerIsCorrect: false)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let ButtonC: AnswerButton = {
+        let button = AnswerButton(frame: .zero, titleText: String(), answerIsCorrect: false)
 
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let ButtonD: AnswerButton = {
+        let button = AnswerButton(frame: .zero, titleText: String(), answerIsCorrect: false)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .orange
-
+        
     }
+    
 }
+
 
