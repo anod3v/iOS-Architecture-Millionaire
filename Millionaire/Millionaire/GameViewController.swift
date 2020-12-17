527efa7657627553490c7e49eab2cb30e89b513d
anod3v <anod3v@gmail.com>
2020-12-17 15:14:17 +0300

    draft version is done

--- Millionaire/Millionaire/GameViewController.swift
+++ Millionaire/Millionaire/GameViewController.swift
@@ -32,7 +32,7 @@ class GameViewController: UIViewController {
     
     let buttonC: AnswerButton = {
         let button = AnswerButton(frame: .zero, titleText: "C", answerIsCorrect: false)
-
+        
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -52,7 +52,7 @@ class GameViewController: UIViewController {
         label.textAlignment = .center
         label.textColor = .white
         label.backgroundColor = .black
-
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -67,18 +67,32 @@ class GameViewController: UIViewController {
     }
     
     func addTapObservers() {
-        self.buttonA.didTapAnswer = { [weak self] _ in
+        self.buttonA.didTapAnswer = { [weak self] response in
             guard let self = self else { return }
-            self.configureQuestions() }
-        self.buttonB.didTapAnswer = { [weak self] _ in
+            self.handleResponse(response: response)
+        }
+        self.buttonB.didTapAnswer = { [weak self] response in
             guard let self = self else { return }
-            self.configureQuestions() }
-        self.buttonC.didTapAnswer = { [weak self] _ in
+            self.handleResponse(response: response)
+        }
+        self.buttonC.didTapAnswer = { [weak self] response in
             guard let self = self else { return }
-            self.configureQuestions() }
-        self.buttonD.didTapAnswer = { [weak self] _ in
+            self.handleResponse(response: response)
+        }
+        self.buttonD.didTapAnswer = {  [weak self] response in
             guard let self = self else { return }
-            self.configureQuestions() }
+            self.handleResponse(response: response)
+        }
+    }
+    
+    func handleResponse(response: Int) {
+        if response == 1 {
+            self.configureQuestions()
+        } else {
+            let welcomeViewController = WelcomeViewController()
+            welcomeViewController.modalPresentationStyle = .fullScreen
+            show(welcomeViewController, sender: nil)
+        }
     }
     
     convenience init() {
@@ -122,31 +136,31 @@ class GameViewController: UIViewController {
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
-        questionLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height / 3),
-        questionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        questionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-        questionLabel.heightAnchor.constraint(equalToConstant: 200),
-        
-        buttonA.topAnchor.constraint(equalTo: questionLabel.bottomAnchor, constant: 26),
-        buttonA.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        buttonA.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant:  -20),
-        buttonA.heightAnchor.constraint(equalToConstant: 20),
-        
-        buttonB.topAnchor.constraint(equalTo: buttonA.bottomAnchor, constant: 26),
-        buttonB.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        buttonB.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-        buttonB.heightAnchor.constraint(equalToConstant: 20),
-        
-        buttonC.topAnchor.constraint(equalTo: buttonB.bottomAnchor, constant: 26),
-        buttonC.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        buttonC.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-        buttonC.heightAnchor.constraint(equalToConstant: 20),
-        
-        buttonD.topAnchor.constraint(equalTo: buttonC.bottomAnchor, constant: 26),
-        buttonD.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        buttonD.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-        buttonD.heightAnchor.constraint(equalToConstant: 20),
-        
+            questionLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height / 3),
+            questionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            questionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            questionLabel.heightAnchor.constraint(equalToConstant: 200),
+            
+            buttonA.topAnchor.constraint(equalTo: questionLabel.bottomAnchor, constant: 26),
+            buttonA.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            buttonA.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant:  -20),
+            buttonA.heightAnchor.constraint(equalToConstant: 20),
+            
+            buttonB.topAnchor.constraint(equalTo: buttonA.bottomAnchor, constant: 26),
+            buttonB.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            buttonB.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            buttonB.heightAnchor.constraint(equalToConstant: 20),
+            
+            buttonC.topAnchor.constraint(equalTo: buttonB.bottomAnchor, constant: 26),
+            buttonC.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            buttonC.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            buttonC.heightAnchor.constraint(equalToConstant: 20),
+            
+            buttonD.topAnchor.constraint(equalTo: buttonC.bottomAnchor, constant: 26),
+            buttonD.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            buttonD.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            buttonD.heightAnchor.constraint(equalToConstant: 20),
+            
         ])
     }
 }
