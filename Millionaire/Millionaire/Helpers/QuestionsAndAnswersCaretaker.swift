44916aabef57dc87fccfe06f06d36f6667caf6cd
anod3v <anod3v@gmail.com>
2020-12-21 18:31:25 +0300

    question is saved ok

---
+++ Millionaire/Millionaire/Helpers/QuestionsAndAnswersCaretaker.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionsAndAnswersCaretaker.swift
+//  Millionaire
+//
+//  Created by Andrey on 21/12/2020.
+//  Copyright © 2020 Andrey Anoshkin. All rights reserved.
+//
+
+import Foundation
